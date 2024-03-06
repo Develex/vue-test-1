c60af42c3f22ecf81c9f03f2e98b254f0120b7be
develex <collinfranckena77@gmail.com>
2024-03-06 14:20:54 +0100

    first commit

---
+++ src/components/Article.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+defineProps(['title'])
+</script>
+
+<template>
+  <div class="flex max-w-full flex-auto bg-teal-100 rounded-md p-8">{{ title }}</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup xmlns="http://www.w3.org/1999/html">
+import Article from "@/components/Article.vue";
+</script>
+
+<template>
+  <main>
+    <article>lol</article>
+    <div class="bg-amber-500 w-max p-8 rounded-md">hi</div>
+  </main>
+</template>
